b410ba7d914b78d9f9f94dd9c0682fd33cb3fc7c
348930405 <348930405@gapps.yrdsb.ca>
2026-02-06 11:48:39 -0500

    67

--- first.py
+++ first.py
@@ -1,2 +1,6 @@
-# This is a comment. The interpreter ignores this line
-print("sup cuh")
+# num1 = 6
+# num2 = 7
+# print(num1 + num2)
+num1 =int(input("enter num 1: "))
+num2 = int(input("enter num 2: "))
+print(num1 + num2)
